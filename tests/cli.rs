1e401a7a88d76c91f3560332153aa9d8c9561478
Saïd <20957603+ayewo@users.noreply.github.com>
2023-10-10 08:46:57 +0100

    Fix lint failure

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,128 +1,138 @@
+use std::process::Command;
+
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
-use std::process::Command;
 
 #[test]
 fn empty_command_args() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("tailcall")?;
+  let mut cmd = Command::cargo_bin("tailcall")?;
 
-    cmd.arg("");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("Usage: tailcall"));
+  cmd.arg("");
+  cmd
+    .assert()
+    .failure()
+    .stderr(predicate::str::contains("Usage: tailcall"));
 
-    Ok(())
+  Ok(())
 }
 
 // Check command tests
 #[test]
 fn check_command_file_not_specified() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("tailcall")?;
+  let mut cmd = Command::cargo_bin("tailcall")?;
 
-    cmd.arg("check");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("error: the following required arguments were not provided"));
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("Usage: tailcall check <FILE_PATH>"));
+  cmd.arg("check");
+  cmd.assert().failure().stderr(predicate::str::contains(
+    "error: the following required arguments were not provided",
+  ));
+  cmd
+    .assert()
+    .failure()
+    .stderr(predicate::str::contains("Usage: tailcall check <FILE_PATH>"));
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn check_command_file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("tailcall")?;
+  let mut cmd = Command::cargo_bin("tailcall")?;
 
-    cmd.arg("check").arg("test.file.doesnt.exist.graphql");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("Failed to read file"));
+  cmd.arg("check").arg("test.file.doesnt.exist.graphql");
+  cmd
+    .assert()
+    .failure()
+    .stderr(predicate::str::contains("Failed to read file"));
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn check_command_file_exists_but_is_invalid() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("tailcall")?;
+  let mut cmd = Command::cargo_bin("tailcall")?;
 
-    cmd.arg("check").arg("tests/graphql/errors/test-const-with-inline.graphql");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("Error: Validation Error"));
+  cmd
+    .arg("check")
+    .arg("tests/graphql/errors/test-const-with-inline.graphql");
+  cmd
+    .assert()
+    .failure()
+    .stderr(predicate::str::contains("Error: Validation Error"));
 
-    Ok(())
+  Ok(())
 }
 
 // Start command tests
 #[test]
 fn start_command_file_not_specified() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("tailcall")?;
+  let mut cmd = Command::cargo_bin("tailcall")?;
 
-    cmd.arg("start");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("error: the following required arguments were not provided"));
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("Usage: tailcall start <FILE_PATH>"));
+  cmd.arg("start");
+  cmd.assert().failure().stderr(predicate::str::contains(
+    "error: the following required arguments were not provided",
+  ));
+  cmd
+    .assert()
+    .failure()
+    .stderr(predicate::str::contains("Usage: tailcall start <FILE_PATH>"));
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn start_command_file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("tailcall")?;
+  let mut cmd = Command::cargo_bin("tailcall")?;
 
-    cmd.arg("start").arg("test.file.doesnt.exist.graphql");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("Error: No such file or directory"));
+  cmd.arg("start").arg("test.file.doesnt.exist.graphql");
+  cmd
+    .assert()
+    .failure()
+    .stderr(predicate::str::contains("Error: No such file or directory"));
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn start_command_log_level() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("tailcall")?;
+  let mut cmd = Command::cargo_bin("tailcall")?;
 
-    cmd.arg("start").arg("--log_level");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("error: unexpected argument '--log_level' found"));
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("Usage: tailcall start <FILE_PATH|--log-level <LOG_LEVEL>>"));
+  cmd.arg("start").arg("--log_level");
+  cmd.assert().failure().stderr(predicate::str::contains(
+    "error: unexpected argument '--log_level' found",
+  ));
+  cmd.assert().failure().stderr(predicate::str::contains(
+    "Usage: tailcall start <FILE_PATH|--log-level <LOG_LEVEL>>",
+  ));
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn start_command_log_level2() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("tailcall")?;
+  let mut cmd = Command::cargo_bin("tailcall")?;
 
-    cmd.arg("start").arg("--log-level");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("error: a value is required for '--log-level <LOG_LEVEL>' but none was supplied"));
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("For more information, try '--help'"));
+  cmd.arg("start").arg("--log-level");
+  cmd.assert().failure().stderr(predicate::str::contains(
+    "error: a value is required for '--log-level <LOG_LEVEL>' but none was supplied",
+  ));
+  cmd
+    .assert()
+    .failure()
+    .stderr(predicate::str::contains("For more information, try '--help'"));
 
-    Ok(())
+  Ok(())
 }
 
 #[test]
 fn start_command_log_level3() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("tailcall")?;
+  let mut cmd = Command::cargo_bin("tailcall")?;
 
-    cmd.arg("start").arg("--log-level").arg("DEBUG");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("error: the following required arguments were not provided"));
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("Usage: tailcall start --log-level <LOG_LEVEL> <FILE_PATH>"));
+  cmd.arg("start").arg("--log-level").arg("DEBUG");
+  cmd.assert().failure().stderr(predicate::str::contains(
+    "error: the following required arguments were not provided",
+  ));
+  cmd.assert().failure().stderr(predicate::str::contains(
+    "Usage: tailcall start --log-level <LOG_LEVEL> <FILE_PATH>",
+  ));
 
-    Ok(())
+  Ok(())
 }
